064b904d5dbc6d969f4d89811e9cc4bfad9acb73
mharrison0224 <mharrison0224@gmail.com>
2016-07-13 11:06:54 -0500

    Delete InstallADDS.ps1

--- InstallADDS.ps1
+++
@@ -1,7 +0,0 @@
-Install-WindowsFeature -Name AD-Domain-Services -IncludeManagementTools -Restart
-
-$NewDisk = Get-Disk | Where-Object PartitionStyle -eq "RAW"
-Initialize-Disk -Number $NewDisk.Number 
-New-Partition -DiskNumber $NewDisk.Number -DriveLetter N -UseMaximumSize
-Format-Volume -DriveLetter N -Confirm:$false -Force
-
